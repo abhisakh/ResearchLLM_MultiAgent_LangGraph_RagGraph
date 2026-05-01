e674548ba0243f26c3d0c99b761b62415699081c
Abhisakh Sarma <abhisakh@gmail.com>
2026-05-01 19:44:54 +0200

    We the the DATABASE URL path from relative to absolute pathto avoid docker confusion

--- backend/backend.py
+++ backend/backend.py
@@ -47,7 +47,8 @@ print(f" {C_GREEN}>> [INIT] Environment variables loaded successfully.{C_RESET}"
 # ------------------------------------------------------------------------------
 # SECTION 2: DATABASE SETUP (SQLite)
 # ------------------------------------------------------------------------------
-DATABASE_URL = "sqlite:///./chat_history.db?check_same_thread=False&timeout=20"
+#DATABASE_URL = "sqlite:///./chat_history.db?check_same_thread=False&timeout=20"
+DATABASE_URL = "sqlite:////app/backend/chat_history.db?check_same_thread=False&timeout=20"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
