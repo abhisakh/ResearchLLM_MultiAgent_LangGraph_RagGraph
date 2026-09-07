227088b8622b7add438c9bd3b4c89aa07fdd4603
Abhisakh Sarma <abhisakh@gmail.com>
2026-09-08 00:05:25 +0200

    Updated the port number for the runder deployment

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 uvicorn backend.main:app --host 0.0.0.0 --port 8000 &
 
 # Start Streamlit frontend on Hugging Face's required port 7860
-streamlit run frontend/ui_main.py --server.port 7860 --server.address 0.0.0.0
+streamlit run frontend/ui_main.py --server.port $PORT --server.address 0.0.0.0
